b916d6fa6a58c8c613cd4a9c438e060c03e9b6fb
Albert Tapia <albertapiadev@gmail.com>
2022-07-01 17:08:38 -0500

    Initial commit

---
+++ src/main/java/com/tapia/student/StudentOauth2Application.java
@@ -0,0 +1,13 @@
+package com.tapia.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tapia/student/StudentOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapia.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
